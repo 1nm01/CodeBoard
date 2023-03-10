27f8ae8521520fe45ffcfdfd68690f7a8f0ec150
Anmol <singhanmol493@gmail.com>
2023-03-10 15:18:42 +0530

    editor changes

--- src/pages/ide/[documentId].js
+++ src/pages/ide/[documentId].js
@@ -1,9 +1,21 @@
 import React from 'react'
+import ReactAce from 'react-ace-editor';
+import Headers from '../../Components/header';
 
 function Ide() {
+
   return (
-    <div>I</div>
+    <div>
+      <Headers/>
+      <ReactAce
+        mode="java"
+        theme="monokai"
+        setReadOnly={false}
+        style={{ height: "100vh" }}
+       // Let's put things into scope
+      />
+    </div>
   )
 }
 
-export default Ide
+export default Ide;
